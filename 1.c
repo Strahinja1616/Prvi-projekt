84644cea5a21801a4a17b4e28facc6733590dfe1
Strahinja1616 <58526312+Strahinja1616@users.noreply.github.com>
2019-12-11 15:50:55 +0100

    novi fajl

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+                        int main()
+{
+
+                            char rijec[10][10]={0};
+                            int i,j;
+                            for(i=0;i<10;i++)
+                            for(j=0;j<10;j++)
+                                printf("%d\n",rijec[i][j]);
+
+                        }
